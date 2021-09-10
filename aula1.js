9883d26372666740d2dfc985e7d4d82d7c1c41dd
SabrinaCintra <sabrinacintra234@gmail.com>
2021-09-10 13:32:32 -0300

    first commit

---
+++ aula1.js
@@ -0,0 +1,19 @@
+nome = "Sabrina"
+idade = 18 
+tel = 11949718999
+endereco = "Rua areado"
+n = 92
+cidade = "carapicuiba"
+cep = 06327170
+cpf = 31097867890
+rg = 398762341
+
+
+
+
+
+
+
+
+console.log("o nome do contato é " + "ele tem " + idade + "anos de idade , o numero do contato dele é " + tel + ", mora no endereco " + endereco + " com o numero " + n " da residencia, na cidade  " + "cep " + cep);
+console.log("numero do cpf: " + cpf + "rg " + rg );
